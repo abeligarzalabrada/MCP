116fe8684e9a87e435406adc5b3fba2de177be62
Facxox <facundo.langone@gmail.com>
2025-07-26 11:56:48 -0300

    Update client.py
    arreglo errores

--- src/client.py
+++ src/client.py
@@ -60,23 +60,6 @@ async def client():
             else:
                 print(response.text)
 
-@mcp.tool(
-    title="Obtener detalles de archivo",
-    description="Retorna metadatos como tamaño, fecha creación y fecha modificación de un archivo específico."
-)
-def obtener_detalles_archivo_tool(path: str):
-    if not os.path.exists(path):
-        return {"error": "El archivo no existe."}
-
-    detalles = {
-        "ruta_absoluta": os.path.abspath(path),
-        "tamaño_bytes": os.path.getsize(path),
-        "fecha_creacion": time.ctime(os.path.getctime(path)),
-        "fecha_modificacion": time.ctime(os.path.getmtime(path)),
-        "es_directorio": os.path.isdir(path),
-        "es_archivo": os.path.isfile(path)
-    }
-
 
 if __name__ == "__main__":
     asyncio.run(client())
